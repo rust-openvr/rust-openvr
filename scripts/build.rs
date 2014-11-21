1786d54f10f71e9660ef5ea33a1fcb922f38be30
Colin Sherratt <colin.sherratt@gmail.com>
2014-11-20 23:07:56 -0500

    lipo for osx

--- scripts/build.rs
+++ scripts/build.rs
@@ -1,4 +1,5 @@
 use std::io::Command;
+#[cfg(target_os = "linux")]
 use std::io::fs;
 
 #[cfg(target_os = "linux")]
@@ -23,9 +24,13 @@ fn main() {
             .arg("build")
             .status()
             .ok().expect("Failed to build");
-    fs::copy(&Path::new("modules/oculus_sdk_mac/LibOVR/Lib/MacOS/Release/libovr.a"),
-             &Path::new(env!("OUT_DIR")).join(Path::new("libovr.a")))
-            .ok().expect("Failed to move file");
+    Command::new("lipo")
+            .arg("modules/oculus_sdk_mac/LibOVR/Lib/MacOS/Release/libovr.a")
+            .arg("-thin")
+            .arg("x86_64")
+            .arg("-output")
+            .arg(Path::new(env!("OUT_DIR")).join(Path::new("libovr.a")).as_str().unwrap())
+            .status()
+            .ok().expect("Failed to lipo library");
     println!("cargo:rustc-flags=-L {} -l ovr:static", env!("OUT_DIR"));
-
 }
